ef62c6fbd464168cc25bc79618bcbe3a014f1b75
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-19 01:02:56 +0700

    Add util library for util function create vectorize_method decorator

---
+++ util/Util.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def vectorize_method(method):
+  """"
+    A decorator for creating a vectorized method inside a class
+  """
+  def vectorized_method(self, *args, **kwargs):
+    return np.vectorize(method)(self, *args, **kwargs)
+  return vectorized_method
