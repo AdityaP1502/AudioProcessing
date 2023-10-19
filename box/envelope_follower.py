64bcd6a7aae57c17b809d5ecc2fbde6decca0f55
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-20 00:26:14 +0700

    Use Numba to optimize smooth function

--- box/envelope_follower.py
+++ box/envelope_follower.py
@@ -1,7 +1,15 @@
 import numpy as np
+from numba import jit
 
 from core.black_box import BlackBox
 
+
+@jit(nopython=True)
+def _smooth(x, prev_val, attack_constant, release_constant):
+    if x > prev_val:
+        return (1 - attack_constant) * x + attack_constant * prev_val
+        
+    return (1 - release_constant) * x + release_constant * prev_val
 
 class EnvelopeFollower(BlackBox):
     def __init__(self, fs, chunks, attack_time=0.05, release_time=3):
@@ -11,16 +19,19 @@ class EnvelopeFollower(BlackBox):
         self._release_constant = np.exp(-2.2 / (44100 * release_time / 1000))
 
     def filter(self):
-        frame = self._input_nodes[0].get_frame()
-        out = np.zeros(len(frame))
-        out[0] = np.abs(frame[0])
+        frame = np.abs(self._input_nodes[0].get_frame())
+        out = np.zeros_like(frame)
+        prev_envelope = np.abs(frame[0])
 
-        for i in range(1, len(frame)):
-            if np.abs(frame[i]) > frame[i - 1]:
-                out[i] = (1 - self._attack_constant) * np.abs(frame[i]) + \
-                    out[i - 1] * self._attack_constant
-            else:
-                out[i] = (1 - self._release_constant) * np.abs(frame[i]) + \
-                    out[i - 1] * self._release_constant
-
+        # s = time()
+        for i, data in enumerate(frame[1:]):
+            out[i + 1] = _smooth(data, prev_envelope, self._attack_constant,
+                    self._release_constant)
+            
+            prev_envelope = out[i + 1]
+                   
+        # _ = _smooth(frame, out, prev_envelope, self._attack_constant, self._release_constant)
+        # e = time()
+        # print((e - s) * 1000, 'ms')
+        
         self._output_node.set_frame(out)
