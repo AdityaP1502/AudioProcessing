800bff0da1d24b4ad7f505c8665648ffedd3038b
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-20 00:26:52 +0700

    Use numba to optimize smooth function

--- box/gain_smooth.py
+++ box/gain_smooth.py
@@ -1,27 +1,30 @@
 import numpy as np
+from numba import jit
 
-from util.Util import vectorize_method
+from time import time
+
 from core.black_box import BlackBox
 
-
+@jit(nopython=True)
+def _smooth(x, prev_val, attack_constant, release_constant):
+    if x < prev_val:
+        return (1 - attack_constant) * x + attack_constant * prev_val
+        
+    return (1 - release_constant) * x + release_constant * prev_val
 class GainSmooth(BlackBox):
     def __init__(self, attack_time: int, release_time: int, fs, chunks):
         super().__init__(fs, chunks)
 
-        self._AT = np.exp(-(1 / (fs * attack_time / 1000)))
-        self._RT = np.exp(-(1 / (fs * release_time / 1000)))
-        self._prev_gain = 1
-
-    @vectorize_method
-    def _smoothing(self, x):
-        if x < self._prev_gain:
-            return (1 - self._AT) * x + self._AT * self._prev_gain
-
-        return (1 - self._RT) * x + self._RT * self._prev_gain
+        self._attack_constant = np.exp(-(1 / (fs * attack_time / 1000)))
+        self._release_constant = np.exp(-(1 / (fs * release_time / 1000)))
+        self._prev_gain : float = 1
 
     def filter(self):
         xn = self._input_nodes[0].get_frame()
-        out = self._smoothing(xn)
-        self._prev_gain = out[-1]
-
+        out = np.zeros_like(xn)
+        for i, data in enumerate(xn):
+            out[i] = _smooth(data, self._prev_gain, self._attack_constant,
+                    self._release_constant)
+            
+            self._prev_gain = out[i]
         self._output_node.set_frame(out)
