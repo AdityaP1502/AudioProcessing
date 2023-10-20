8ccf2e1da36cdf524cd1f14ea3c1fc677276030d
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-20 17:49:13 +0700

    Add new class method to create a new component

--- core/cascaded_box.py
+++ core/cascaded_box.py
@@ -7,6 +7,17 @@ class CascadedBox():
             call the insert_components method to assign the component to the class instance. 
           """
 
+    @classmethod
+    def create_system_from_components(cls, *args):
+        """
+        Create a system by cascading each components
+        from the argument. 
+        """
+        
+        instance = cls()
+        instance.insert_components(*args)
+        return instance
+    
     def insert_components(self, *args):
         args = list(args)
         args.sort(key=lambda x: x[0])
@@ -16,15 +27,25 @@ class CascadedBox():
     def insert_input_node_to_component(self, node, component_id):
         self._components[component_id].insert_input_node(node)
 
-    def get_output_node(self):
-        if self._output_node is None:
-            return self._components[-1].get_output_node
+    def insert_input_node(self, node):
+        self._components[0].insert_input_node(node)
+        
+    def get_output_node(self, id=None):
+        if id == None:
+            if self._output_node is None:
+                return self._components[-1].get_output_node()
 
-        return self._output_node
+            return self._output_node
+        
+        return self._components[id].get_output_node()
 
     def set_output_node(self, node):
         self._output_node = node
 
     def filter(self):
-        for component in self._components:
+        for _, component in enumerate(self._components):
+            # s = time()
             component.filter()
+            # e = time()
+            
+            # print(f"{_} component take {(e - s) * 1000} ms to finish")
