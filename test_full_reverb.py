a937edb4e76328247be50a318029003836a97bae
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-21 22:17:49 +0700

    Refactor code to follow template

--- test_full_reverb.py
+++ test_full_reverb.py
@@ -1,35 +1,39 @@
 import os.path
+import traceback
+import sys
 
 from core.nodes import Nodes
 from core.cascaded_box import CascadedBox
 
 from box.arithmetic import Adder
-
 from reverb.reverb_box import ReverbBox
 from reverb.tapped_delay_line import TappedDelayLine
 
 from testing.wav_file_io import save_wav, read_wav
+from testing.test import Test
 
+MIX = 0.75
+CHUNK_SIZE=None
+SAMPLE_RATE=None
 
 if __name__ == "__main__":
-    mix = 0.75
     dst = os.path.join('temp', 'temp.wav')
     obj = read_wav(dst)
 
     data = obj["data"]
     
-    chunk_size = obj["chunks"]
-    fs = obj["sample_rate"]
+    CHUNK_SIZE = obj["chunks"]
+    SAMPLE_RATE = obj["sample_rate"]
     
     # initiate the component here
-    input_node = Nodes(chunks=chunk_size)
-    input_node.set_frame_exceed_chunk_length((1 - mix) * data)
+    input_node = Nodes(chunks=CHUNK_SIZE)
+    input_node.set_frame_exceed_chunk_length((1 - MIX) * data)
     
     delay_line = TappedDelayLine(
         delay=[10, 30, 40, 50, 75, 100, 125, 250, 500], 
         gain=[0.95, 0.85, 0.87, 0.7, 0.8, 0.82, 0.69, 0.69, 0.5],
-        fs=fs,
-        chunks=chunk_size
+        fs=SAMPLE_RATE,
+        chunks=CHUNK_SIZE
     )
     
     reverb_component = ReverbBox(delay=[
@@ -48,9 +52,9 @@ if __name__ == "__main__":
         0.8,
         0.71,
         0.698,
-    ], fs=fs, chunks=chunk_size)
+    ], fs=SAMPLE_RATE, chunks=CHUNK_SIZE)
 
-    adder = Adder(fs=fs, chunks=chunk_size)
+    adder = Adder(fs=SAMPLE_RATE, chunks=CHUNK_SIZE)
 
     reverb_component.insert_input_node(delay_line.get_tap(-2))
 
@@ -58,13 +62,25 @@ if __name__ == "__main__":
     adder.insert_input_node(reverb_component.get_output_node())
 
     component = CascadedBox.create_system_from_components((0, delay_line), (1, reverb_component), (2, adder))
-
     component.insert_input_node(input_node)
-    n_iter = len(data) // obj["chunks"]
+    
 
-    for i in range(n_iter):
-        component.filter()
-        data[i * chunk_size : (i + 1) * chunk_size] = mix * data[i * chunk_size : (i + 1) * chunk_size] + \
-            component.get_output_node().get_frame()
+    test = Test(
+        component=component,
+        data=MIX * data,
+        chunks=CHUNK_SIZE,
+        verbose=True
+    )
 
+    try:
+        test.start()
+        test.join()
+    except Exception as e:
+        traceback.print_exception(e)
+        print("Terminate testing without saving data")
+        sys.exit(1)
+    
+    obj["data"] = test.data
+    print('Saving data into ./temp/result.wav')
     save_wav(dst=os.path.join('temp', 'result.wav'), **obj)
+    print('Done.')
