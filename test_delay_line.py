8c7d7f7115d12b9003022ddbd853330bfd549440
AdityaP1502 <i.m.aditya015@gmail.com>
2023-10-21 22:14:18 +0700

    change fs into sample_rate

--- test_delay_line.py
+++ test_delay_line.py
@@ -7,42 +7,42 @@ from reverb.tapped_delay_line import TappedDelayLine
 from testing.wav_file_io import save_wav, read_wav
 from testing.test import Test
 
+MIX = 0.75
+CHUNK_SIZE = None
+SAMPLE_RATE = None
 
 if __name__ == "__main__":
-    mix = 0.75
     dst = os.path.join('temp', 'temp.wav')
     obj = read_wav(dst)
 
     data = obj["data"]
+    CHUNK_SIZE = obj["chunks"]
+    SAMPLE_RATE = obj["sample_rate"]
 
-    chunk_size = obj["chunks"]
-    fs = obj["sample_rate"]
+    input_node = Nodes(chunks=CHUNK_SIZE)
+    input_node.set_frame_exceed_chunk_length((1 - MIX) * data)
 
     # initiate the component here
-    input_node = Nodes(chunks=chunk_size)
-    input_node.set_frame_exceed_chunk_length((1 - mix) * data)
-
+    ##############################
+    
+    ## Changes delay and gain parameter here, make sure they
+    ## are the same length
+    
     delay_line = TappedDelayLine(
-        delay=[10, 30, 40, 50, 75, 100, 125, 250, 500], 
+        delay=[10, 30, 40, 50, 75, 100, 125, 250, 500],
         gain=[0.95, 0.85, 0.87, 0.7, 0.8, 0.82, 0.69, 0.69, 0.5],
-        fs=fs,
-        chunks=chunk_size
+        fs=SAMPLE_RATE,
+        chunks=CHUNK_SIZE
     )
-
+    ############################
+    
     component = delay_line
-
     component.insert_input_node(input_node)
-    # n_iter = len(data) // obj["chunks"]
-
-    # for i in range(n_iter):
-    #     component.filter()
-    #     data[i * chunk_size : (i + 1) * chunk_size] = mix * data[i * chunk_size : (i + 1) * chunk_size] + \
-    #         component.get_output_node().get_frame()
     
     test = Test(
         component=component,
-        data=data,
-        chunks=chunk_size,
+        data=MIX * data,
+        chunks=CHUNK_SIZE,
         verbose=True
     )
 
@@ -53,7 +53,8 @@ if __name__ == "__main__":
         traceback.print_exception(e)
         print("Terminate testing without saving data")
         sys.exit(1)
-        
+    
+    obj["data"] = test.data
     print('Saving data into ./temp/result.wav')
     save_wav(dst=os.path.join('temp', 'result.wav'), **obj)
     print('Done.')
